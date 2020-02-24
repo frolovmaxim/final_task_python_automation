2a58570eff45f19ae321aa41b08cd2024e66063f
maxim <frolov_max86@mail.ru>
2020-02-24 10:40:22 +0300

    add transition between pages

--- pages/locators.py
+++ pages/locators.py
@@ -5,4 +5,4 @@ class MainPageLocators():
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_REGISTER = (By.CSS_SELECTOR, "#register_form")
